412dc514d190892edb2bed7c467adbb5e0e19297
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-08-18 17:15:48 -0300

    -m

---
+++ src/main/java/com/projetospring/ProjetoWebSpring/ProjetoWebSpringApplication.java
@@ -0,0 +1,15 @@
+package com.projetospring.ProjetoWebSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebSpringApplication.class, args);
+	}
+
+        
+        
+}
